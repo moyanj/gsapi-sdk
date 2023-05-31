60fbd9a57ea2ecee0c687244e5f192bd376cb644
moyan <1561515308@qq.com>
2023-05-31 20:54:26 +0800

    Create gssdk.js

---
+++ js/gssdk.js
@@ -0,0 +1,20 @@
+//ZAJ.js库代码
+
+	(function (){	    //注册命名空间 'AZJ' 到window对象上  
+
+        window['gsapi'] = {} 
+
+		
+
+	    //$函数等同于getElementByID；
+		  
+
+          
+
+		  //把创建的函数$注册到 'window.AZJ'命名空间中
+
+		  window['gsapi']['$']=$;
+
+          
+
+		})();
